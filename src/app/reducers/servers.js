f6706d90e6884a8a56aee639e28f5aeba2200aa2
Manuel Rodriguez <rodriguez.manu@gmail.com>
2016-08-07 18:24:42 -0300

    fix eslint

--- src/app/reducers/servers.js
+++ src/app/reducers/servers.js
@@ -3,23 +3,25 @@ import {initialStateServer} from '../constants/InitialStates';
 
 export default function servers(state = initialStateServer, action) {
   switch (action.type) {
-    case ADD_SERVER:
+    case ADD_SERVER: {
       return [
         ...state,
         {
           id: state.reduce((maxId, server) => Math.max(server.id, maxId), -1) + 1,
           used: false
-        },
+        }
       ];
+    }
 
-    case DELETE_SERVER:
-      var last = state.pop();
+    case DELETE_SERVER: {
+      const last = state.pop();
 
       return state.map(server =>
         server.id === last.id ?
           Object.assign({}, server, {used: !server.used}) :
           server
       );
+    }
     default:
       return state;
   }
